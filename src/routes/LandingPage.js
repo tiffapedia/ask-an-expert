dc92e6fd833f034dedfeea9db3a45dfbfbaf6964
Tiffany J <tiffapedia@gmail.com>
2020-02-20 13:27:43 -0800

    complete thank you page

--- src/routes/LandingPage.js
+++ src/routes/LandingPage.js
@@ -10,14 +10,13 @@ class LandingPage extends React.Component {
   render() {
     return (
       <div>
-        <div className='block'>
+        <div className='block-1'>
           <Col xsOffset={1} xs={10} sm={10} md={10} lg={10}>
             <div className='flex-container'>
               <h4>Not what you expected?</h4>
               <a href='/form' className='ml-1em'>Ask an expert</a>
             </div>
           </Col>
-
         </div>
         <hr />
       </div>
--- src/routes/ThankYouPage.js
+++ src/routes/ThankYouPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Row, Col } from 'react-flexbox-grid';
 
 class ThankYouPage extends React.Component {
   render() {
     return (
-      <div>Ask an expert</div>
+      <div>
+        <div className='block-1p5'>
+          <Col xsOffset={1} xs={10} sm={10} md={10} lg={10}>
+            <div>
+              <h3>Thank you.</h3>
+              <div className='gray-txt'>An expert will get back to you soon.</div>
+            </div>
+          </Col>
+        </div>
+        <hr />
+      </div>
     )
   }
 
